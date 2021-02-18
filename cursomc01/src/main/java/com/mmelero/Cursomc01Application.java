77b5c7df2b2b7b11717c1acd8558d22bfa54e67f
Marcos Melero <mmelero@gmail.com>
2021-02-18 20:43:33 -0300

    Efetuando a copia do prosjto anterior

---
+++ cursomc01/src/main/java/com/mmelero/Cursomc01Application.java
@@ -0,0 +1,13 @@
+package com.mmelero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmelero/Cursomc01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmelero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
